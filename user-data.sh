9d04c2519fa76bd971ce310fb5d4e7053ee6f155
Shubham Dixit <shubhamdixit765@gmail.com>
2026-09-16 12:45:06 +0530

    Update user-data.sh

--- user-data.sh
+++ user-data.sh
@@ -15,3 +15,17 @@ KeepAliveTimeout 65
 MaxKeepAliveRequests 100
 EOF
 
+systemctl restart httpd
+
+# Create application directories
+mkdir -p /var/www/html/app1
+
+# Create application pages
+echo "<h1>Welcome to App 1</h1>" > /var/www/html/app1/index.html
+echo "<h1>Welcome to Root App</h1>" > /var/www/html/index.html
+
+# Download and install CloudWatch Agent
+wget https://amazoncloudwatch-agent.s3.amazonaws.com/redhat/amd64/latest/amazon-cloudwatch-agent.rpm
+
+rpm -U ./amazon-cloudwatch-agent.rpm
+
