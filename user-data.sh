40f68533d4d24ab70d124150c217a9c4738feddd
Shubham Dixit <shubhamdixit765@gmail.com>
2026-09-16 12:44:45 +0530

    Update user-data.sh

--- user-data.sh
+++ user-data.sh
@@ -7,3 +7,11 @@ dnf install -y httpd wget
 # Enable and start Apache
 systemctl enable --now httpd
 
+# Configure Apache KeepAlive
+cat >> /etc/httpd/conf/httpd.conf <<'EOF'
+
+KeepAlive On
+KeepAliveTimeout 65
+MaxKeepAliveRequests 100
+EOF
+
